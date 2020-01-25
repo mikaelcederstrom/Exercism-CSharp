2eb366cee5a50a37a98b87271741d2d6341b40b6
Mikael Cederström <mikael.cederstrom@gmail.com>
2020-01-25 15:45:51 +0100

    init commit

---
+++ 2.1_isogram/Isogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        var list = new List<string>();
+        
+        foreach (var item in word)
+        {
+            string l = item.ToString().ToLower();
+            if (!list.Contains(l) || Regex.IsMatch(l, @"[ _-]+$"))
+                list.Add(l);
+            else 
+                return false;
+        }
+        return true;
+    }
+}
