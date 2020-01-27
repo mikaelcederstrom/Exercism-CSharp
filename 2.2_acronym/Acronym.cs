39b09121394ddae83dec44a5a97e35243560eb77
Mikael Cederström <mikael.cederstrom@gmail.com>
2020-01-27 19:47:14 +0100

    Init commit, Acronym

---
+++ 2.2_acronym/Acronym.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+        phrase = phrase.ToUpper();
+        phrase = phrase.Trim('\'');
+        string[] words = phrase.Split(' ', '-', '_');        
+        string acronym = "";
+
+        foreach (var word in words)
+        {
+            if (Regex.IsMatch(word, @"[A-Z]"))
+                acronym = acronym + word[0];
+        }
+        return acronym;
+    }
+}
